fd4a9ef91cfa5889a4cfe9b49e67d6344d6a0331
naufal <naufalfidha2003@gmail.com>
2024-11-25 11:58:09 +0100

    test commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        String var = "This will be commited as a test!";
+        System.out.println(var);
     }
 }
